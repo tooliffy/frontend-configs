874f8d6416954ab50a6c62a3698663ca72f29308
zubov_f <zubov_f@tochka.com>
2025-07-25 01:00:39 +0300

    ci: добавлена стадия установки зависимостей

--- eslint/index.js
+++ eslint/index.js
@@ -1,7 +1,35 @@
 const config = {
+  root: true,
   env: {
-    node: true,
+    browser: true,
     es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/react-in-jsx-scope": "off",
+
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
 
